8d4042a417e9f7cf2fef9341db8191443da75d2b
Avikam Mangla <avikam03@gmail.com>
2020-03-30 23:29:29 +0530

    Update and rename AMAZING QUESTION.py to q1.py

--- q1.py
+++ q1.py
@@ -1,4 +1,4 @@
-#AMAZING QUESTION
+#question1
 '''
 Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 '''
